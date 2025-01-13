106dd24173502f526f48ff28ca4943b152bdd8b0
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-01-13 19:39:54 +0300

    Поправил сборку

--- app/src/main/java/com/example/messenger/InputFragment.kt
+++ app/src/main/java/com/example/messenger/InputFragment.kt
@@ -1,33 +1,22 @@
 package com.example.messenger
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.text.format.Formatter
 import android.view.View
-import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.messenger.databinding.FragmentChatBinding
 import com.example.messenger.databinding.FragmentInputBinding
-import com.example.messenger.databinding.FragmentMainBinding
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.runBlocking
 
 class InputFragment : Fragment(R.layout.fragment_input) {
     private lateinit var binding: FragmentInputBinding
-    private val viewModel by lazy {
-        val provider = ViewModelProvider(owner = this)
-        provider[MainFragmentViewModel::class.java]
-    }
+    private lateinit var viewModel: MainFragmentViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentInputBinding.bind(view)
+
+        val dao = StorageApp.db.messageItemDao()
+        val factory = MainFragmentViewModelFactory(dao)
+        viewModel = ViewModelProvider(this, factory)[MainFragmentViewModel::class.java]
 
         initEventListeners()
     }
